4d5e247fe0901c479c1a2d3ad51efb9aa5cc016e
Gabriel <alexandro.jimenezg@gmail.com>
2026-04-06 17:51:59 +0200

    new images

--- data.js
+++ data.js
@@ -17,16 +17,20 @@ const WEDDING = {
   /* ── Hero Background Images (crossfade slideshow) ───────── */
   // Add as many images as you want — they'll cycle with a smooth fade
   heroImages: [
-    "https://drive.google.com/thumbnail?id=13Sd6yifjhnSYglSinkO7BWiUnh3B_z6H&sz=w1024", // corazon
+    //"https://drive.google.com/thumbnail?id=13Sd6yifjhnSYglSinkO7BWiUnh3B_z6H&sz=w1024", // corazon
+    "https://drive.google.com/thumbnail?id=1AaIlXiZ6AQmHI6MEu99_wE6BphMcESzD&sz=w1024", // corazon
 
-    "https://drive.google.com/thumbnail?id=1zSBP9Ytjql5g5Lx684BNDLPMbGNVxJNV&sz=w1024", // mirando al sol
+    //"https://drive.google.com/thumbnail?id=1zSBP9Ytjql5g5Lx684BNDLPMbGNVxJNV&sz=w1024", // mirando al sol
+    "https://drive.google.com/thumbnail?id=1QYMReDjcJobQ9ppZHcRhbiZiavM4dPJh&sz=w1024", // mirando al sol
 
     //"https://drive.google.com/thumbnail?id=1s9SLuHB9wahUu_fmZ9iGvTyfm3suJSqr&sz=w1024", // charango
     //"https://drive.google.com/thumbnail?id=17LXsw5a11DgHGcvJKMWFlVrkAaim5MSA&sz=w1024",
-    "https://drive.google.com/thumbnail?id=1ouhcElSPvfTTzKTOVixWSugmTH5ssPue&sz=w1024",
+    //"https://drive.google.com/thumbnail?id=1ouhcElSPvfTTzKTOVixWSugmTH5ssPue&sz=w1024",
+    "https://drive.google.com/thumbnail?id=1kTvNNkiLdcN-Ml9K8CiEmLRLtoIR7mBS&sz=w1024",
 
     //"https://drive.google.com/thumbnail?id=1wlL9o-tRq4FzGoGQZMPWrEayPJFICIAW&sz=w1024", // foto de los dos
-    "https://drive.google.com/thumbnail?id=1ig2nhsSG2xzzXLKtIBalfqqzirqeH7zU&sz=w1024", // flor
+    //"https://drive.google.com/thumbnail?id=1ig2nhsSG2xzzXLKtIBalfqqzirqeH7zU&sz=w1024", // flor
+    "https://drive.google.com/thumbnail?id=1UEFSI6kg61zHkWexIBouJVK6LmBb43f8&sz=w1024",
   ],
 
   /* ── Date & Time ────────────────────────────────────────── */
